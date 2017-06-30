83f13a6c8c484275fa176d854b40c7f3179a4ac7
ginsongsong <ginsongsong@honghutech.com>
2017-06-30 18:57:04 +0800

    Update CPU_MAX.sh

--- CPU_MAX.sh
+++ CPU_MAX.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+ apt-get install   linux-tools-4.xxx.xx lm-sensors  fancontrol
+echo "CPU freq list"
+cpupower frequency-info 
 echo "Set CPU freq"
 #3.6GHz
  echo  3600000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
- 
+
 echo "Set CPU memory"
  
 
