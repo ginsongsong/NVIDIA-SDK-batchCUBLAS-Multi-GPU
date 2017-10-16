f3e6cc89b4e86342216c9360fe4dc0fda073f611
ginsongsong <ginsongsong@honghutech.com>
2017-10-15 22:26:01 -0400

    Update CPU_MAX.sh

--- CPU_MAX.sh
+++ CPU_MAX.sh
@@ -13,3 +13,10 @@ echo "Set CPU memory"
  
 
 echo "Set CPU fan speed..."
+
+
+#pycaffe
+#apt-get install python-pip python-dev
+#pip install --upgrade numpy spicy pillow
+#or
+#for req in $(cat requirements.txt); do pip install $req; done
